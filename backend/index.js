1fc56271f7270a465db17e13d4db170029febe2e
aruma19 <muhammadalmasfarrosd@gmail.com>
2025-03-03 20:13:31 +0700

    menghilangkan import route

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import cors from "cors";
-import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json()); //menerima req dalam format json
-app.use(UserRoute);
 
 app.listen(5002, ()=>  
     console.log('Server up and running...'));  
